29f8f490813f675f4b8bbc21e8f44c46b9c4c5e8
Egor Lord_F Shcherbin <javamanse@ya.ru>
2017-02-16 20:04:29 +0700

    Moved LazyImplConcurrent to LazyFactory

--- hw1/src/main/java/LazyFactory.java
+++ hw1/src/main/java/LazyFactory.java
@@ -6,10 +6,35 @@ public class LazyFactory {
     }
 
     public static <T> Lazy<T> createLazyConcurrent(Supplier<T> supplier) {
-        return null;
+        return new LazyImplConcurrent<>(supplier);
     }
 
     public static <T> Lazy<T> createLazyConcurrentLockFree(Supplier<T> supplier) {
         return null;
+    }
+
+    private static class LazyImplConcurrent<T> implements Lazy<T> {
+        private static Object noResultYet = new Object();
+
+        private Supplier<T> supplier;
+        private volatile T result = (T) noResultYet;
+
+        private LazyImplConcurrent(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            if (result != noResultYet) {
+                return result;
+            }
+            synchronized (this) {
+                if (result != noResultYet) {
+                    result = supplier.get();
+                    supplier = null;
+                }
+            }
+            return result;
+        }
     }
 }
