f29c07c0f6f8514184ad81b0fc072d877b56109a
Egor Lord_F Shcherbin <javamanse@ya.ru>
2017-03-16 04:03:33 +0700

    Added some primitive CLI

--- hw2/src/main/java/ru/spbau/eshcherbin/nucleus/Main.java
+++ hw2/src/main/java/ru/spbau/eshcherbin/nucleus/Main.java
@@ -1,7 +1,30 @@
 package ru.spbau.eshcherbin.nucleus;
 
+import ru.spbau.eshcherbin.nucleus.vcs.DirectoryExpectedException;
+import ru.spbau.eshcherbin.nucleus.vcs.NucleusManager;
+import ru.spbau.eshcherbin.nucleus.vcs.RepositoryAlreadyInitializedException;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        if (Objects.equals(args[0], "init")) {
+            Path path = Paths.get("").toAbsolutePath();
+            if (args.length >= 2) {
+                path = path.resolve(args[1]).normalize();
+            }
+            try {
+                NucleusManager.initRepository(path);
+            } catch (IOException e) {
+                System.out.println("IO Error");
+            } catch (DirectoryExpectedException e) {
+                System.out.println("Directory expected");
+            } catch (RepositoryAlreadyInitializedException e) {
+                System.out.println("Repository already initialized");
+            }
+        }
     }
 }
