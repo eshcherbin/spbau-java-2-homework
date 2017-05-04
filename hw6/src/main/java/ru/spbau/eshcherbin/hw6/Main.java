5bb2c3b2028acad4a06f541c2e3572985f90ccd0
Egor Shcherbin <javamanse@ya.ru>
2017-05-04 19:03:24 +0700

    Initial commit

---
+++ hw6/src/main/java/ru/spbau/eshcherbin/hw6/Main.java
@@ -0,0 +1,13 @@
+package ru.spbau.eshcherbin.hw6;
+
+/**
+ * Main class of the application.
+ */
+public class Main {
+    /**
+     * Starts the application.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
