0254ebb1832ef26e36878e5da7ad86e183af17f5
Egor Shcherbin <javamanse@ya.ru>
2017-05-14 18:40:05 +0700

    Added simple tests

---
+++ hw6/src/test/java/ru/spbau/eshcherbin/hw6/myjunit/MyTestRunnerTest.java
@@ -0,0 +1,52 @@
+package ru.spbau.eshcherbin.hw6.myjunit;
+
+import org.junit.Test;
+import ru.spbau.eshcherbin.hw6.myjunit.mytestingclasses.MySimpleTestingClass;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MyTestRunnerTest {
+    @Test
+    public void simpleTest() throws Exception {
+        final MyTestRunner testRunner = new MyTestRunner(MySimpleTestingClass.class);
+        final List<MyTestReport> reports = testRunner.runTests();
+        reports.sort(Comparator.comparing(MyTestReport::getTestName));
+        assertThat(reports.size(), is(5));
+
+        final MyTestReport report0 = reports.get(0);
+        assertThat(report0.getClassName(), is(MySimpleTestingClass.class.getName()));
+        assertThat(report0.getTestName(), is("expectedExceptionTest"));
+        assertThat(report0 instanceof MyTestOkReport, is(true));
+        assertThat(report0.isSuccessful(), is(true));
+
+        final MyTestReport report1 = reports.get(1);
+        assertThat(report1.getClassName(), is(MySimpleTestingClass.class.getName()));
+        assertThat(report1.getTestName(), is("ignoredTest"));
+        assertThat(report1 instanceof MyTestIgnoredReport, is(true));
+        assertThat(report1.isSuccessful(), is(true));
+
+        final MyTestReport report2 = reports.get(2);
+        assertThat(report2 instanceof MyTestNoExceptionReport, is(true));
+        assertThat(report2.getClassName(), is(MySimpleTestingClass.class.getName()));
+        assertThat(report2.getTestName(), is("noExceptionTest"));
+        assertThat(report2.isSuccessful(), is(false));
+
+        final MyTestReport report3 = reports.get(3);
+        assertThat(report3.getClassName(), is(MySimpleTestingClass.class.getName()));
+        assertThat(report3.getTestName(), is("simpleTest"));
+        assertThat(report3 instanceof MyTestOkReport, is(true));
+        assertThat(report3.isSuccessful(), is(true));
+
+        final MyTestReport report4 = reports.get(4);
+        assertThat(report4.getClassName(), is(MySimpleTestingClass.class.getName()));
+        assertThat(report4.getTestName(), is("unexpectedExceptionTest"));
+        assertThat(report4 instanceof MyTestUnexpectedExceptionReport, is(true));
+        assertThat(report4.isSuccessful(), is(false));
+        assertThat(((MyTestUnexpectedExceptionReport) report4).getException().getClass(),
+                is(NullPointerException.class));
+    }
+}
